15b4a738f42e6ea4d9ec1a76d0fd635173d5de97
Ben Grinthal <bgrinthal@gmail.com>
2023-05-07 16:56:03 -0400

    added notes for clarity

--- client/src/controller/review.controller.js
+++ client/src/controller/review.controller.js
@@ -3,8 +3,10 @@ import reviewModel from "../models/review.model";
 
 const create = async (req, res) => {
     try {
+        // object destructure setting req.params to movieId
         const { movieId } = req.params
 
+        //  create a new instance of reviewModel
         const review = await new reviewModel({
             user: req.user.id,
             movieId, 
@@ -13,6 +15,7 @@ const create = async (req, res) => {
 
         await review.save();
 
+        // sends response with given info
         responseHandler.created(res, {
             ...review._doc,
             id: review.id,
@@ -27,11 +30,13 @@ const remove = async (req, res) => {
     try {
         const { reviewId } = req.params
 
+        // looks for review with given information
         const review = await reviewModel.findOne({
             _id: reviewId,
             user: req.user.id
         })
 
+        // if cannot find
         if (!review) return responseHandler.notfound(res)
 
         await review.remove();
@@ -45,6 +50,7 @@ const remove = async (req, res) => {
 
 const getReviewsOfUser = async (req, res) => {
     try {
+        // finds all of the reviews of given user
         const reviews = await reviewModel.find({
             user: req.user.id,
         }).sort("-createdAt");
